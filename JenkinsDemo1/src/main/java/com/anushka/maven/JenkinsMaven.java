7526aaecb6d7e57273a65c11666d7e526c1217c5
Anushka <anushkan1996@gmail.com>
2021-07-29 16:23:31 +0530

    Add JenkinsDemo1

---
+++ JenkinsDemo1/src/main/java/com/anushka/maven/JenkinsMaven.java
@@ -0,0 +1,11 @@
+package com.anushka.maven;
+
+public class JenkinsMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins");
+
+	}
+
+}
